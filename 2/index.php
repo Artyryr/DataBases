27803988c713f75d4bd99698d13366930a1e5087
Artyryr <ArtyryrF@mail.ru>
2016-05-03 12:18:26 +0300

    Databases

---
+++ 2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+</style>
+<title>Registration</title>
+</head>
+
+
+
+<body>
+
+	
+<form action="registration.php" method="POST">
+Email:
+<input type="text" id="email" name="email" />
+Password:
+<input type="password" id="password" name="password" />
+
+<input name="submit" id="submit" type="submit" value="Register" />
+</form>
+<a href="login.html">Log In</a>
+</body>
+</html>
